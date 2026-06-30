5d5380176a7c5a9c0787440ae7e079cdf3c7d63f
Francois Laagel <f.laagel@ieee.org>
2026-06-30 12:39:44 +0200

    src/06_forth/forth.asm: - reclaim two bytes of EEPROM from the '0=' (NULP) implementation.   This probably could apply to other comparison operators. Also it is ground   work for ANS94 compatible exception support. - set version to '20260630'.

--- SW/src/forth.asm
+++ SW/src/forth.asm
@@ -2543,11 +2543,10 @@ NULP	fcb	2		ANSI (Core)
 	tfr	x,d
 	tfr	0,x
 	tstd
-	beq	@nulp2
+	bne	@nulp1
+	leax	-1,x		Return the ANSI true
 @nulp1	UCNPUSH
 	rts
-@nulp2	leax	-1,x		Return the ANSI true
-	bra	@nulp1
 
 ZNEQ	fcb	3		ANSI (Core ext)
 	fcc	'0<>'
@@ -4832,7 +4831,7 @@ BOOTMSG	fcb	CR,LF
 	fcc	'Z79Forth/AI 6309 ANS Forth System'
 	ENDC			RTCFEAT
 	fcb	CR,LF
-	fcc	'20251028 (C) Francois Laagel 2019'
+	fcc	'20260630 (C) Francois Laagel 2019'
 	fcb	CR,LF,CR,LF,NUL
 
 RAMOKM	fcc	'RAM OK: 32 KB'
